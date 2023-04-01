f141f7754b417144f92a4221c3a36173eb5f23da
Atul3007 <atuldwivedi859@gmail.com>
2023-04-01 21:45:50 +0530

    day-5

---
+++ Frontend/Scripts/login.js
@@ -0,0 +1,24 @@
+    let form =document.querySelector("form");
+form.addEventListener("submit",async (e)=>{
+    e.preventDefault();
+    let obj={
+        pass:form.exampleFormControlInput2.value,
+        email:form.exampleFormControlInput1.value
+    }
+    try {
+        let res=await fetch("http://localhost:8080/user/signin",{
+            method:"POST",
+            headers:{
+                'Content-type':'application/json'
+            },
+            body:JSON.stringify(obj)
+        })
+        let data=await res.json();
+        console.log(data);
+        localStorage.setItem("token",data.token);
+       //  alert("Login Successful");
+        // window.location.href="notes.html";
+      } catch (error) {
+        console.log(error);
+      }
+})
