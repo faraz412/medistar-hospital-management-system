c381c6746f8c940a2b78cb8d76b1cbe8cc575396
faraz412 <ahmedfaraz412@gmail.com>
2023-05-13 00:45:41 +0530

    baseURL updated

--- Frontend/Scripts/baseURL.js
+++ Frontend/Scripts/baseURL.js
@@ -1,5 +1,7 @@
 // let baseURL="http://localhost:8080/";
 
-let baseURL="https://gorgeous-ruby-jaguar.cyclic.app/";
+// let baseURL="https://gorgeous-ruby-jaguar.cyclic.app/";
+
+let baseURL="https://medistar-backend.onrender.com/";
 
 export default baseURL;
